6462a19a1e71b1d72fab6f39a57363620919eb8a
Vladimir Vorobiev <vvs-it@bk.ru>
2021-09-13 22:52:51 +0600

    Подключение webpack

---
+++ webpack.dev.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist')
+  },
+  mode: 'development',
+  devServer: {
+    open: true,
+    port: 8080,
+    hot: true,
+    writeToDisk: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
